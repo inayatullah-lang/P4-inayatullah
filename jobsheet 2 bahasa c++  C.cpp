304ff193559de0aa0eac5b24bfdb88bd70d7757f
inayatullah-lang <65220151+inayatullah-lang@users.noreply.github.com>
2020-05-12 02:21:23 -0700

    Create jobsheet 2 bahasa c++  C.cpp
    Menampilkan array 2 dimensi

---
+++ jobsheet 2 bahasa c++  C.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Inayatullah\n\n";
+	cout<<"Nim: F1B019071\n\n";
+	cout<<"Kelompok 29\n\n";
+	string nim[11];
+	
+	cout<<"Masukkan nilai 1 : "; cin>>nim[0];
+	cout<<"\nMasukkan nilai 2 : "; cin>>nim[1];
+	cout<<"\nMasukkan nilai 3 : "; cin>>nim[2];
+	cout<<"\nMasukkan nilai 4 : "; cin>>nim[3];
+	cout<<"\nMasukkan nilai 5 : "; cin>>nim[4];
+	cout<<"\nMasukkan nilai 6 : "; cin>>nim[5];
+	cout<<"\nMasukkan nilai 7 : "; cin>>nim[6];
+	cout<<"\nMasukkan nilai 8 : "; cin>>nim[7];
+	cout<<"\nMasukkan nilai 9 : "; cin>>nim[8];
+	cout<<"\nMasukkan nilai 10 : "; cin>>nim[9];
+	cout<<"\nMasukkan nilai 11 : "; cin>>nim[10];
+	
+	cout<<endl;
+	
+	cout<< "Data NIM Mahasisawa yang anda input : "<<endl;
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "\nNim ke-2 adalah " << nim[1] << endl;
+	cout << "\nNim ke-3 adalah " << nim[2] << endl;
+	cout << "\nNim ke-4 adalah " << nim[3] << endl;
+	cout << "\nNim ke-5 adalah " << nim[4] << endl;
+	cout << "\nNim ke-6 adalah " << nim[5] << endl;
+	cout << "\nNim ke-7 adalah " << nim[6] << endl;
+	cout << "\nNim ke-8 adalah " << nim[7] << endl;
+	cout << "\nNim ke-9 adalah " << nim[8] << endl;
+	cout << "\nNim ke-10 adalah " << nim[9] << endl;
+	cout << "\nNim ke-11 adalah " << nim[10] << endl;
+	
+	return 0;
+
+}
