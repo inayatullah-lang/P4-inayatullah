ef6a32c818703a36f0068848dd0e2fd089cc4a3d
inayatullah-lang <65220151+inayatullah-lang@users.noreply.github.com>
2020-05-12 02:18:31 -0700

    Create jobsheet 1 bahasa c++  C.cpp
    Menampilkan array 1 dimensi

---
+++ jobsheet 1 bahasa c++  C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{	
+	cout<<"Inayatullah\n\n";
+	cout<<"Nim: F1B019071\n\n";
+	cout<<"kelompok 29\n\n";
+int nim[11]= {1,7,9,2,7};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[9] << endl;
+	cout << "Nim ke-8 adalah " << nim[3] << endl;
+	cout << "Nim ke-9 adalah " << nim[6] << endl;
+	cout << "Nim ke-10 adalah " << nim[2] << endl;
+	cout << "Nim ke-11 adalah " << nim[4] << endl;	
+
+}
